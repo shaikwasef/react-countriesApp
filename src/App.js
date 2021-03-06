8fd21944c303d4efa4c2c4c0cc0cf9d42728841c
shaikwasef <wasefshaik@gmail.com>
2021-03-06 13:47:21 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import axios from "axios";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state({countryList : []});
+    this.state = { countryList: [] };
   }
 
   async componentDidMount() {
-    const countryListNames = await axios.get("https://restcountries.eu/rest/v2/all");
-    countryListNames.data.forEach((value) => {this.setState({countryList : this.state.countryList.concat(value.name)})});
+    const countryListNames = await axios.get(
+      "https://restcountries.eu/rest/v2/all"
+    );
+    countryListNames.data.forEach(value => {
+      this.setState({ countryList: this.state.countryList.concat(value.name) });
+    });
   }
 
   render() {
-    return <select>{countryNames}</select>;
+    const countryNames = this.state.countryList.map((value, key) => {
+      return <option key={key}>{value}</option>;
+    });
+    return <select className={nameList}>{countryNames}</select>;
   }
 }
 
